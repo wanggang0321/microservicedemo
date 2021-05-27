9b5ac15dfd52f17e523347618522f261b60b323d
wanggang <578620382@qq.com>
2021-05-28 00:08:39 +0800

    高可用注册中心

---
+++ eureka2-demo/src/test/java/com/leyou/eureka2demo/Eureka2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyou.eureka2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eureka2DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
