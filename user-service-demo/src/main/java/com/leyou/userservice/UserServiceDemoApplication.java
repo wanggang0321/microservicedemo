98a37822155f50b32a61319565744b154fb5b602
wanggang <578620382@qq.com>
2021-05-27 23:32:50 +0800

    1.添加Eureka注册中心 2.将user-service注册到eureka

--- user-service-demo/src/main/java/com/leyou/userservice/UserServiceDemoApplication.java
+++ user-service-demo/src/main/java/com/leyou/userservice/UserServiceDemoApplication.java
@@ -2,8 +2,10 @@ package com.leyou.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient // 开启EurekaClient功能
 public class UserServiceDemoApplication {
 
     public static void main(String[] args) {
