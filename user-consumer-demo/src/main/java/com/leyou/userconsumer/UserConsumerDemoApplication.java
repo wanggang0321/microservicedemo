b957c014129d6944d484ee9745ec2d91bcab782c
wanggang <578620382@qq.com>
2021-05-27 23:55:24 +0800

    消费者从Eureka获取服务

--- user-consumer-demo/src/main/java/com/leyou/userconsumer/UserConsumerDemoApplication.java
+++ user-consumer-demo/src/main/java/com/leyou/userconsumer/UserConsumerDemoApplication.java
@@ -2,11 +2,13 @@ package com.leyou.userconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient // 开启EurekaClient功能
 public class UserConsumerDemoApplication {
 
     @Bean
--- user-consumer-demo/src/main/java/com/leyou/userconsumer/controller/ConsumerController.java
+++ user-consumer-demo/src/main/java/com/leyou/userconsumer/controller/ConsumerController.java
@@ -2,6 +2,7 @@ package com.leyou.userconsumer.controller;
 
 import com.leyou.userconsumer.pojo.User;
 import com.leyou.userconsumer.service.UserService;
+import com.leyou.userconsumer.service.UserService2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +17,12 @@ public class ConsumerController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserService2 userService2;
 
     @GetMapping
     public List<User> consume(@RequestParam("ids") List<Long> ids) {
-        return userService.queryUserByIds(ids);
+        return userService2.queryUserByIds(ids);
     }
 
 }
