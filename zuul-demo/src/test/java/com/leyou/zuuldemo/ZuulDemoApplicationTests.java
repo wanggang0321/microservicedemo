9072391f71fe0dbae5e6b369f21aad3587702323
wanggang <890321wang>
2021-05-28 18:00:09 +0800

    添加Zuul,实现面向服务的路由

---
+++ zuul-demo/src/test/java/com/leyou/zuuldemo/ZuulDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyou.zuuldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
